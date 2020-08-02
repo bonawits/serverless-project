13b7d27c0f9f2b8689ea53164df0ce162cd45ebd
Craig Sloane <craigrussellsloane@gmail.com>
2020-08-02 21:47:34 +0100

    Add lambda for creating todos

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,38 @@
-import 'source-map-support/register'
+import "source-map-support/register";
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from "aws-lambda";
 
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { CreateTodoRequest } from "../../requests/CreateTodoRequest";
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+import { getUserId } from "../utils";
+import { TodosAccess } from "../../dataLayer/todosAccess";
+import { createLogger } from "../../utils/logger";
+
+const logger = createLogger("createTodo");
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
 
   // TODO: Implement creating a new TODO item
-  return undefined
-}
+  const userId = getUserId(event);
+  logger.info(`event: ${event}`);
+  logger.info(`userId: ${userId}`);
+  const item = await new TodosAccess().createTodo(newTodo, userId);
+
+  return {
+    statusCode: 201,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      item,
+    }),
+  };
+};

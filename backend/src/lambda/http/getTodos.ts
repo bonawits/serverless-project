2337302c5882a83387f5f926d7569e919428176e
Craig Sloane <craigrussellsloane@gmail.com>
2020-08-02 21:47:48 +0100

    Update lambda for getting todos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,33 @@
-import 'source-map-support/register'
+import "source-map-support/register";
+import { getUserId } from "../utils";
+import { getAllTodos } from "../../businessLogic/todos";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler,
+} from "aws-lambda";
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-}
+import { createLogger } from "../../utils/logger";
+
+const logger = createLogger("getTodos");
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const user = getUserId(event);
+  const items = await getAllTodos(user);
+
+  logger.info(`create group for user ${user}`);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      items,
+    }),
+  };
+};
